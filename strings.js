5528ebec9016ac317cd90406c8aeccc03695b8d2
yejinyoun <yejinyoun@icloud.com>
2023-02-06 15:05:20 +0100

    string template exercise

--- strings.js
+++ strings.js
@@ -1,10 +1,1 @@
 "use strict";
-
-let name = "Jini";
-
-let animal = "dog";
-
-let animalName = "Ttori";
-
-console.log(`My name is ${name}.
-I have a ${animal} called ${animalName}.`);
---
+++ template.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let name = "Jini";
+
+let animal = "dog";
+
+let animalName = "Ttori";
+
+console.log(`My name is ${name}.
+I have a ${animal} called ${animalName}.`);
