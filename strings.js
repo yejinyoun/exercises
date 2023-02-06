64c3e3d6284117de9271265dab70b4de68f6a9a9
yejinyoun <yejinyoun@icloud.com>
2023-02-06 15:49:51 +0100

    substrings and trim

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const name = "Pipi";
+const name = "Pipi Popo Papaya";
 const drink = "pepsi";
 
 const text = `Hello ${name}, 
@@ -15,3 +15,21 @@ console.log(`${name} is ${len} characters long.`);
 const letter = name[0];
 
 console.log(`First Letter of ${name} is ${letter}`);
+
+const str1 = "    There is    space here \n   ";
+const str2 = str1.trim();
+
+console.log(str2);
+
+/* trim() removes spaces(even \n, \t) on the beginning and the end. */
+
+/* trick
+console.log(`_${variable}_`) to check if there was any space.
+*/
+
+const firstName = name.substring(0, 4);
+
+console.log(`_${firstName}_`);
+
+const lastName = name.substring(10);
+console.log(`_${lastName}_`);
