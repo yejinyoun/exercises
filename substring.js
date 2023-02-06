283b1be11e8bb75a2c6ea85559dbb713bade0ece
yejinyoun <yejinyoun@icloud.com>
2023-02-06 15:58:19 +0100

    substring exercise

---
+++ substring.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const name = "Albus Percival Wulfric Brian Dumbledore";
+
+const answer1 = name.substring(0, 5);
+const answer2 = name.substring(29);
+const answer3 = name.substring(15, 22);
+const answer4 = name.substring(14, 23);
+const answer5 = name.substring(25, 28);
+const answer6 = name.substring(2, 5);
+
+console.log(`_${answer6}_`);
