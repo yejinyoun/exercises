03d9e1646eac445919a8d8782f11e563be5fda3c
yejinyoun <yejinyoun@icloud.com>
2023-02-06 15:28:40 +0100

    string length and index

--- strings.js
+++ strings.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const name = "Pipi";
+const drink = "pepsi";
+
+const text = `Hello ${name}, 
+would you like a ${drink}?`;
+
+console.log(text);
+
+const len = name.length;
+
+console.log(`${name} is ${len} characters long.`);
+
+const letter = name[0];
+
+console.log(`First Letter of ${name} is ${letter}`);
