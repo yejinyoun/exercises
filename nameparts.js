b7b23b7c8c0b7912d407742809b480b9ac009155
yejinyoun <yejinyoun@icloud.com>
2023-02-07 13:40:07 +0100

    nameparts exercise set up

---
+++ nameparts.js
@@ -0,0 +1,16 @@
+"use strict";
+/* Given a name string, e.g. “Peter Heronimous Lind”
+
+- split the string into three variables: firstName, middleName and lastName.
+
+Hint: use indexOf and substring 
+
+Expect the name to be a const - you can’t modify it.
+
+Console.log the three variables at the very end of your code. */
+
+const name = "Peter Heronimous Lind";
+
+let firstName;
+let middleName;
+let lastName;
