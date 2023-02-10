e2458110bef903dcf456cc38ea0c06bb3912e16b
yejinyoun <yejinyoun@icloud.com>
2023-02-10 21:40:02 +0100

    object exercise

--- object.js
+++ object.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const person1 = {
+/* const person1 = {
   firstName: "Peter",
   age: 29,
   student: false,
@@ -43,3 +43,60 @@ person1.firstName = person2.firstName;
 // this works, in this way u can change the value of a property
 
 console.log(person1);
+ */
+
+// exercise1
+
+const person1 = {
+  firstName: "Peter",
+  age: 43,
+  student: false,
+};
+
+console.log(person1.lastName);
+
+person1.lastName = "Tolstrup";
+console.log(person1.lastName);
+
+person1.lastName = undefined;
+console.log(person1.lastName);
+console.log("person1.Middlename:", person1.middleName);
+
+delete person1.lastName;
+// setting it to undefined still keeps the property and value is undefined,
+// but if you delete it, it removes the property
+
+person1.age++;
+console.log(person1.age);
+
+const person2 = person1;
+// Reference : the object is the same, but the variables are different!
+// object values are passed by references
+// two variables are referring - pointing - to the same object
+// person1 is a reference to student2
+
+person2.firstName = "Bob";
+
+console.log(person1);
+
+// person1 = person2; -> doesn't work bc person1 is declared with const
+
+const student1 = {
+  firstName: "Harry",
+  lastName: "Potter",
+};
+
+const student2 = {
+  firstName: "Harry",
+  lastName: "Potter",
+};
+
+student1.firstName = student2.firstName;
+student1.lastName = student2.lastName;
+
+if (student1 == student2) {
+  console.log("same");
+} else {
+  console.log("not same");
+}
+// not same
