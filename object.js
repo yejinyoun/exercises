73225fc190ce546be218fa8504bf8d27b99c3510
yejinyoun <yejinyoun@icloud.com>
2023-02-10 20:30:25 +0100

    object exercise

--- object.js
+++ object.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const person1 = {
+  firstName: "Peter",
+  age: 29,
+  student: false,
+};
+
+console.log(person1.lastName);
+
+person1.lastName = "Lind";
+console.log(person1.lastName);
+
+person1.lastName = undefined;
+
+console.log(person1.lastName);
+console.log(person1.middleName);
+console.log(person1);
+
+delete person1.lastName;
+
+console.log(person1);
+
+// delete removes property completely
+// but setting it to = undefined, property remains but with value of undefined
+
+person1.age++;
+
+console.log(person1.age);
+console.log(person1);
+
+const person2 = {
+  firstName: "Jin",
+  age: 22,
+  student: true,
+};
+
+console.log(person2);
+
+// person1 = person2; -> does not work
+
+person1.firstName = person2.firstName;
+// this works, in this way u can change the value of a property
+
+console.log(person1);
