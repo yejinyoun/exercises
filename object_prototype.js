3e120e0fb314d725a4a74b900065b883906bfc03
yejinyoun <yejinyoun@icloud.com>
2023-02-10 23:24:52 +0100

    object prototype exercise

--- object_prototype.js
+++ object_prototype.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("hihi");
+
+const Animal = {
+  name: "",
+  type: "unknown",
+  desc: "",
+  age: 0,
+};
+
+// prototype names start with upper case by convention.
+// can set to default values
+
+const animal = Object.create(Animal);
+// can create actual objects like this
+// object names start with lower case by convention
+
+// -> animal.name = name;
+// animal.type = type;
+// animal.desc = desc;
+
+// but they can be overwritten in the object without modifying the prototype
+
+Animal.image = "image.jpg";
+
+console.log(animal.image);
