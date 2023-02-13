32c1c7691a0800f44b93c326b2ab5b9f8ed15477
yejinyoun <yejinyoun@icloud.com>
2023-02-13 16:15:47 +0100

    full name function (connected with name part function)

--- fullname_function.js
+++ fullname_function.js
@@ -36,3 +36,50 @@ function fullName(lastName, firstName, middleName) {
 fullName("Potter", "Harry", "James");
 
  */
+
+// connected with getNameParts function
+// (be careful !!) don't name the function and parameter same;
+
+getNameParts("Harry Potter");
+
+function getNameParts(fullName) {
+  // if the fullName inclues more than 1 spacebar
+  if (fullName.includes(" ", fullName.indexOf(" ") + 1) == true) {
+    let firstName = fullName.substring(0, fullName.indexOf(" "));
+
+    console.log(`_${firstName}_`);
+
+    let middleName = fullName.substring(
+      fullName.indexOf(" ") + 1,
+      fullName.indexOf(" ", fullName.indexOf(" ") + 1)
+    );
+    console.log(`_${middleName}_`);
+
+    let lastName = fullName.substring(fullName.indexOf(" ", fullName.indexOf(" ") + 1) + 1);
+
+    console.log(`_${lastName}_`);
+
+    getFullName(lastName, firstName, middleName);
+  } else {
+    //if it only includes 1 or less spacebar
+    let firstName = fullName.substring(0, fullName.indexOf(" "));
+    let lastName = fullName.substring(fullName.indexOf(" ") + 1);
+    let middleName = undefined;
+
+    console.log(`_${firstName}_`);
+    console.log(`_${lastName}_`);
+    console.log(`_${middleName}_`);
+
+    getFullName(lastName, firstName, middleName);
+  }
+}
+
+function getFullName(lastName, firstName, middleName) {
+  if (middleName !== undefined) {
+    let entireName = `${firstName} ${middleName} ${lastName}`;
+    console.log(entireName);
+  } else {
+    let entireName = `${firstName} ${lastName}`;
+    console.log(entireName);
+  }
+}
