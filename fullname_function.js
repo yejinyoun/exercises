11f66331278f05e48c14eaf4b411066742b283d0
yejinyoun <yejinyoun@icloud.com>
2023-02-13 15:48:03 +0100

    full name function (ignoring middlename if it doesnt exist)

--- fullname_function.js
+++ fullname_function.js
@@ -1,3 +1,38 @@
 "use strict";
 
 console.log("hihi");
+
+// function if there's no parameter of middleName, ignore it
+
+/* 
+
+//1
+
+function fullName(lastName, firstName, middleName) {
+  if (middleName !== undefined) {
+    let newString = `${firstName} ${middleName} ${lastName}`;
+
+    console.log(newString);
+  } else {
+    let newString = `${firstName} ${lastName}`;
+    console.log(newString);
+  }
+}
+
+fullName("Potter", "Harry", "James");
+
+//2
+
+function fullName(lastName, firstName, middleName) {
+  if (middleName == undefined) {
+    let newString = `${firstName} ${lastName}`;
+    console.log(newString);
+  } else {
+    let newString = `${firstName} ${middleName} ${lastName}`;
+    console.log(newString);
+  }
+}
+
+fullName("Potter", "Harry", "James");
+
+ */
