9d19b07e14c7bf0912172c13c94f0f99c573a326
yejinyoun <yejinyoun@icloud.com>
2023-02-13 15:09:04 +0100

    capitalization function 1.parameter and argument 2.parameter and variable

--- capitalization_function.js
+++ capitalization_function.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log("hihi");
+// caplitalization
+// string.substring(0,1).toUpperCase() + string.substring(1).toLowerCase();
+
+/* 
+
+let namestring = "jininiNNNi";
+
+let newstring = namestring.substring(0, 1).toUpperCase() + namestring.substring(1).toLowerCase();
+
+console.log(newstring);
+ */
+
+//make function w parameter and string argument
+
+/* function capitalize(str) {
+  let newstr = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+  console.log(newstr);
+} */
+
+/* capitalize("tToRi"); */
+
+// with variable
+
+function capitalize(str) {
+  let newstr = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+  console.log(newstr);
+}
+
+const name = "tToRI";
+
+capitalize(name);
