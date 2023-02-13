3dd15c586aa391ffc3024d3ce2e6554a69ae21ab
yejinyoun <yejinyoun@icloud.com>
2023-02-13 11:58:33 +0100

    exercise 1ab

--- function_parameter.js
+++ function_parameter.js
@@ -1,3 +1,26 @@
 "use strict";
 
-console.log("hihi");
+/* function sayHello(firstName) {
+    console.log(`Hello ${firstName}`);
+  } */
+
+/* function sayHello(firstName) {
+  console.log(`Hello`, firstName);
+} */
+
+/* function sayHello(firstName) {
+  console.log("Hello " + firstName);
+} */
+
+//exercise 1a 1b
+
+/* const firstName = "jin";
+const lastName = "jini";
+
+sayHello(lastName);
+
+function sayHello(firstName, lastName) {
+  console.log(`Hello ${firstName} ${lastName}`);
+} */
+
+/* sayHello("jin"); */
