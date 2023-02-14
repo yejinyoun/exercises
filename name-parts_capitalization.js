6df51756a80701870d629ef8de39a08a6b8c828a
yejinyoun <yejinyoun@icloud.com>
2023-02-14 21:44:20 +0100

    name-parts & capitalization exercise basic js structure

--- name-parts_capitalization.js
+++ name-parts_capitalization.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hihi");
+
+// separate fullname (parameter) into firstName, middleName, lastName
+// capitalize each
+// return an object
+
+const fullname = "Harry James Potter";
+
+console.log(getNameParts(fullname));
+
+function getNameParts() {}
+
+function capitalize() {}
