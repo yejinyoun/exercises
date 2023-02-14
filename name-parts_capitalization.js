e2afbd64071323bd5379da9b227edbc2648a03dc
yejinyoun <yejinyoun@icloud.com>
2023-02-14 22:27:43 +0100

    getNameParts function with split (case1)

--- name-parts_capitalization.js
+++ name-parts_capitalization.js
@@ -4,10 +4,26 @@
 // capitalize each
 // return an object
 
-const fullname = "Harry James Potter";
+const fullName = "Harry James Potter";
 
-console.log(getNameParts(fullname));
+getNameParts(fullName);
 
-function getNameParts() {}
+function getNameParts(fullname) {
+  // case 1. firstname + middlename + lastname
+  if (fullname.split(" ").length >= 3) {
+    let firstName = fullname.split(" ")[0];
+    let middleName = fullname
+      .split(" ")
+      .slice(1, fullname.split(" ").length - 1) //returns new array (startpoint,endpoint)
+      .join(" "); //convert into string, using " " in between elements
+    let lastName = fullname.split(" ")[fullname.split(" ").length - 1];
+    console.log(lastName);
+    console.log(`_${middleName}_`);
+    console.log(typeof middleName);
+  }
+  // else
+}
 
 function capitalize() {}
+
+function fullnameToArray() {}
