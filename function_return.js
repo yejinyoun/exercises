1d2ceef5f24ae0fd91ffa42c65b801810704e900
yejinyoun <yejinyoun@icloud.com>
2023-02-14 12:46:22 +0100

    function return exercise 2

--- function_return.js
+++ function_return.js
@@ -9,3 +9,14 @@ function greeting(firstName) {
 const result = greeting("peter");
 
 console.log(result);
+
+//exercise 2
+
+greeting("mimi");
+console.log(greeting("mimi"));
+
+console.log(greeting());
+
+const txt = `Greeting is ${greeting("a name")}`;
+
+console.log(txt);
