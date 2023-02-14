dd0bd66902b1fc20e1da6790a4e6533a0db601c9
yejinyoun <yejinyoun@icloud.com>
2023-02-14 13:21:34 +0100

    full name return without capitalization function

--- fullname_return.js
+++ fullname_return.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const firstNameVar = "Harry";
+const middleNameVar = "James";
+const lastNameVar = "Potter";
+// putting return value in a variable
+const fullFullName = fullName(lastNameVar, firstNameVar, middleNameVar);
+
+console.log(fullFullName);
+
+//function of making a full name
+function fullName(lastName, firstName, middleName) {
+  if (middleName == undefined) {
+    let newString = `${firstName} ${lastName}`;
+    return newString;
+  } else {
+    let newString = `${firstName} ${middleName} ${lastName}`;
+    return newString;
+  }
+}
