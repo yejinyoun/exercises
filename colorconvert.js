50e1ca6efb3227ec39d161d535a77523294e916a
yejinyoun <yejinyoun@icloud.com>
2023-02-15 17:12:38 +0100

    rgb to hex w return

--- colorconvert.js
+++ colorconvert.js
@@ -1,5 +1,4 @@
 "use strict";
-console.log("hihi");
 
 //make a function that converts "rbg(255, 99, 71)"" to r : "255", so on
 //steps parameter - cssColor (string)
@@ -7,7 +6,7 @@ console.log("hihi");
 // turn it into an array ["num1", "num2", "num3"]
 // set each variable r,g,b and value is - array item(string), and trim, turn into number
 
-cssToRGB("rbg(255, 99, 71)");
+/* cssToRGB("rbg(255, 99, 71)");
 
 function cssToRGB(cssColor) {
   let rgb = cssColor.slice(4, cssColor.indexOf(")"));
@@ -23,12 +22,12 @@ function cssToRGB(cssColor) {
 
   console.log(`_${b}_`);
   console.log(typeof b);
-}
+} */
 
 //hexToRGB
 //#ff6347 - digit 1-2 = red, 3-4 = green, 5-6 = blue
 
-hexToRGB("#ff6347");
+/* hexToRGB("#ff6347");
 
 function hexToRGB(hexColor) {
   let hex = hexColor.substring(1); // to remove # on the front
@@ -40,7 +39,7 @@ function hexToRGB(hexColor) {
   console.log(`_${r}_`);
   console.log(`_${g}_`);
   console.log(`_${b}_`);
-}
+} */
 
 // rgbToHEX
 // take each r,g,b value(number) as parameter
@@ -49,7 +48,7 @@ function hexToRGB(hexColor) {
 // if the converted value is less than 2digit, - (1digit) so add 0 in front
 // add all together and put # on the front
 
-rgbToHEX(9, 0, 37);
+/* rgbToHEX(9, 0, 37);
 
 function rgbToHEX(r, g, b) {
   let red = r.toString(16);
@@ -71,4 +70,38 @@ function rgbToHEX(r, g, b) {
 
   console.log(hex);
   console.log(typeof hex);
+} */
+
+// with return
+// instead of let hex = value inside the function,
+// defined hex outside and the value is function return
+
+let rgb = {
+  r: 9,
+  g: 0,
+  b: 37,
+};
+
+let hex = rgbToHEX(rgb);
+
+console.log(`_${hex}_`);
+console.log(typeof hex);
+
+function rgbToHEX(input) {
+  let red = input.r.toString(16);
+  if (red.length < 2) {
+    red = "0" + red;
+  }
+
+  let green = input.g.toString(16);
+  if (green.length < 2) {
+    green = "0" + green;
+  }
+
+  let blue = input.b.toString(16);
+  if (blue.length < 2) {
+    blue = "0" + blue;
+  }
+
+  return "#" + red + green + blue;
 }
