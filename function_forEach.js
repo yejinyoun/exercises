6940bff4c9ddce10e8817b273cc0d5abacd9be4c
yejinyoun <yejinyoun@icloud.com>
2023-02-16 10:20:04 +0100

    function foreach with arrow function

--- function_forEach.js
+++ function_forEach.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("hihi");
+
+const people = ["Harry", "Ron", "Hermione", "Neville"];
+
+function sayHello(person) {
+  console.log(`Hello ${person}`);
+}
+
+people.forEach(sayHello);
+
+//same as
+people.forEach((person) => console.log(`Hello ${person}`));
