718e428042d3e36eef6713c1698c67fca87c2d5b
yejinyoun <yejinyoun@icloud.com>
2023-02-16 10:34:26 +0100

    checking parameters of forEach

--- function_forEach.js
+++ function_forEach.js
@@ -10,3 +10,11 @@ people.forEach(sayHello);
 
 //same as
 people.forEach((person) => console.log(`Hello ${person}`));
+
+//testParam
+function testParams(a, b, c, d) {
+  console.log(`a:${a}, b:${b}, c:${c}, d:${d}`);
+}
+
+people.forEach(testParams);
+// forEach takes 3 parameters - each item (value), index of each item, entire array
